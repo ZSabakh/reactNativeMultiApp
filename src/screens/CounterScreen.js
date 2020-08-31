2d5a0f59d64bbfb26c618defc79d9adc8d85e3e4
ZSabakh <zurabsabakhtarishvili@gmail.com>
2020-08-30 19:58:59 -0500

    Dispatch testing

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+function reducer(state, action) {
+  switch (action.math) {
+    case "+":
+      return { ...state, counter: state.counter + action.amount };
+    case "-":
+      return { ...state, counter: state.counter - action.amount };
+    default:
+      return state;
+  }
+}
+
 function CounterScreen() {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0); Nonono
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
 
   return (
     <View>
       <Button
         title="More"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ math: "+", amount: 1 });
         }}
       />
       <Button
         title="Less"
         onPress={() => {
-          setCounter(counter - 1);
+          // setCounter(counter - 1); DAMPALI SABJOTA SHEMORCHENILI (Jk, dis method is fine af, just playing with reducers)
+          dispatch({ math: "-", amount: 1 });
         }}
       />
       <Text>C(o)unt: {counter}</Text>
